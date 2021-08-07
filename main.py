5ac25e3ed710fbf2fa7bb7d3fe987639a3fc0fd2
pietroclemente92 <pietroclemente92@hotmail.com>
2021-08-07 10:19:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,7 @@ def commands(update: Update, context: CallbackContext) -> None:
             InlineKeyboardButton("Regolamento Cacio", callback_data='/regolamento'),
         ],
         [
-            InlineKeyboardButton("Timeout Formazione", callback_data='/timeout_formazione'),
-            InlineKeyboardButton("Timeout Formazione", callback_data='/timeout_formazione'),
+            InlineKeyboardButton("Timeout Formazione", callback_data='/timeout_formazione')
         ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
